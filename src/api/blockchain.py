ce66c5693d29a6c5b2ce5561ec39d72fd0765f4c
Josh C <jcremeans90@gmail.com>
2022-11-03 23:00:37 -0400

    Add new error code 522, add continue_on_error API flag

--- src/api/blockchain.py
+++ src/api/blockchain.py
@@ -5,8 +5,8 @@ import src.enums
 # API Doc: https://www.blockchain.com/api/blockchain_api
 # API Doc: https://api.blockchain.com/v3/
 class Blockchain(src.api.base_api.BaseAPI):
-    def get_current_price(self, symbol: str):
-        request = src.api.base_api.BaseAPI.request_data(self, src.enums.APIClient.BLOCKCHAIN_V3, 'tickers', endpoint_args=symbol)
+    def get_current_price(self, symbol: str, continue_on_error: bool = False):
+        request = src.api.base_api.BaseAPI.request_data(self, src.enums.APIClient.BLOCKCHAIN_V3, 'tickers', endpoint_args=symbol, continue_on_error=continue_on_error)
         if request is None:
             return None
 
